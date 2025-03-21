d84d13db7b31148fea30258c0adec2cb18bbc3f2
Kumaraguru <19eucs071@skcet.ac.in>
2025-03-21 08:52:21 +0000

    fixing bootstrap node sprs

--- src/handlers/nodeHandlers.js
+++ src/handlers/nodeHandlers.js
@@ -71,7 +71,14 @@ export async function runCodex(config, showNavigationMenu) {
             const executable = config.codexExe;
             const args = [
                 `--data-dir="${config.dataDir}"`,
+                `--storage-quota=11811160064`,
                 `--api-cors-origin="*"`,
+                `--bootstrap-node=spr:CiUIAhIhAiJvIcA_ZwPZ9ugVKDbmqwhJZaig5zKyLiuaicRcCGqLEgIDARo8CicAJQgCEiECIm8hwD9nA9n26BUoNuarCEllqKDnMrIuK5qJxFwIaosQ3d6esAYaCwoJBJ_f8zKRAnU6KkYwRAIgM0MvWNJL296kJ9gWvfatfmVvT-A7O2s8Mxp8l9c8EW0CIC-h-H-jBVSgFjg3Eny2u33qF7BDnWFzo7fGfZ7_qc9P`,
+                `--bootstrap-node=spr:CiUIAhIhAyUvcPkKoGE7-gh84RmKIPHJPdsX5Ugm_IHVJgF-Mmu_EgIDARo8CicAJQgCEiEDJS9w-QqgYTv6CHzhGYog8ck92xflSCb8gdUmAX4ya78QoemesAYaCwoJBES39Q2RAnVOKkYwRAIgLi3rouyaZFS_Uilx8k99ySdQCP1tsmLR21tDb9p8LcgCIG30o5YnEooQ1n6tgm9fCT7s53k6XlxyeSkD_uIO9mb3`,
+                `--bootstrap-node=spr:CiUIAhIhA7E4DEMer8nUOIUSaNPA4z6x0n9Xaknd28Cfw9S2-cCeEgIDARo8CicAJQgCEiEDsTgMQx6vydQ4hRJo08DjPrHSf1dqSd3bwJ_D1Lb5wJ4Qt_CesAYaCwoJBEDhWZORAnVYKkYwRAIgFNzhnftocLlVHJl1onuhbSUM7MysXPV6dawHAA0DZNsCIDRVu9gnPTH5UkcRXLtt7MLHCo4-DL-RCMyTcMxYBXL0`,
+                `--bootstrap-node=spr:CiUIAhIhAzZn3JmJab46BNjadVnLNQKbhnN3eYxwqpteKYY32SbOEgIDARo8CicAJQgCEiEDNmfcmYlpvjoE2Np1Wcs1ApuGc3d5jHCqm14phjfZJs4QrvWesAYaCwoJBKpA-TaRAnViKkcwRQIhANuMmZDD2c25xzTbKSirEpkZYoxbq-FU_lpI0K0e4mIVAiBfQX4yR47h1LCnHznXgDs6xx5DLO5q3lUcicqUeaqGeg`,
+                `--bootstrap-node=spr:CiUIAhIhAgybmRwboqDdUJjeZrzh43sn5mp8jt6ENIb08tLn4x01EgIDARo8CicAJQgCEiECDJuZHBuioN1QmN5mvOHjeyfmanyO3oQ0hvTy0ufjHTUQh4ifsAYaCwoJBI_0zSiRAnVsKkcwRQIhAJCb_z0E3RsnQrEePdJzMSQrmn_ooHv6mbw1DOh5IbVNAiBbBJrWR8eBV6ftzMd6ofa5khNA2h88OBhMqHCIzSjCeA`,
+                `--bootstrap-node=spr:CiUIAhIhAntGLadpfuBCD9XXfiN_43-V3L5VWgFCXxg4a8uhDdnYEgIDARo8CicAJQgCEiECe0Ytp2l-4EIP1dd-I3_jf5XcvlVaAUJfGDhry6EN2dgQsIufsAYaCwoJBNEmoCiRAnV2KkYwRAIgXO3bzd5VF8jLZG8r7dcLJ_FnQBYp1BcxrOvovEa40acCIDhQ14eJRoPwJ6GKgqOkXdaFAsoszl-HIRzYcXKeb7D9`,
                 `--log-level=DEBUG`,
                 `--log-file="${logFilePath}"`,
                 `--storage-quota="${config.storageQuota}"`,
@@ -83,9 +90,9 @@ export async function runCodex(config, showNavigationMenu) {
                 `--bootstrap-node=spr:CiUIAhIhAiJvIcA_ZwPZ9ugVKDbmqwhJZaig5zKyLiuaicRcCGqLEgIDARo8CicAJQgCEiECIm8hwD9nA9n26BUoNuarCEllqKDnMrIuK5qJxFwIaosQ3d6esAYaCwoJBJ_f8zKRAnU6KkYwRAIgM0MvWNJL296kJ9gWvfatfmVvT-A7O2s8Mxp8l9c8EW0CIC-h-H-jBVSgFjg3Eny2u33qF7BDnWFzo7fGfZ7_qc9P`
             ];
 
-            const command = 
+            const command =
                 `"${executable}" ${args.join(" ")}`
-            
+
             console.log(showInfoMessage(
                 '🚀 Codex node is running...\n\n' +
                 'If your firewall ask, be sure to allow Codex to receive connections. \n' +
@@ -117,7 +124,7 @@ export async function runCodex(config, showNavigationMenu) {
 
                     // Start periodic logging
                     const stopLogging = await startPeriodicLogging(config);
-                    
+
                     nodeProcess.on('exit', () => {
                         stopLogging();
                     });
@@ -223,7 +230,7 @@ async function showNodeDetails(data, showNavigationMenu) {
         case '3':
             try {
                 const existingWallet = await getWalletAddress();
-                
+
                 console.log(boxen(
                     `${chalk.cyan('Current wallet address:')}\n${existingWallet || 'Not set'}`,
                     {
@@ -258,14 +265,14 @@ export async function checkNodeStatus(config, showNavigationMenu) {
             const spinner = createSpinner('Checking node status...').start();
             const response = await runCommand(`curl http://localhost:${config.ports.apiPort}/api/codex/v1/debug/info`);
             spinner.success();
-            
+
             const data = JSON.parse(response);
-            
+
             const peerCount = data.table.nodes.length;
             const isOnline = peerCount > 2;
-            
+
             console.log(boxen(
-                isOnline 
+                isOnline
                     ? chalk.green('Node is ONLINE & DISCOVERABLE')
                     : chalk.yellow('Node is ONLINE but has few peers'),
                 {
